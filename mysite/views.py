5cb0f44d1bc116361e5a06606b1376cc82341601
satyam srivastava <satyamsrivastav435@gmail.com>
2022-08-20 17:11:29 +0530

    Project Completed

--- mysite/views.py
+++ mysite/views.py
@@ -5,66 +5,70 @@ def index(request):
     return render(request, "index.html")
 
 def analyze(request):
-    data = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    removespaces = request.GET.get('spaceremover', "off")
-    countchar = request.GET.get('countchar','off')
+    data = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    removespaces = request.POST.get('spaceremover', "off")
+    countchar = request.POST.get('counter','off')
 
     #
     # REMOVE PUNCTUATION MARKS
     #
     if removepunc == 'on':
-        punctuations = ''' !()-[]{};:'"\,<>./?@#$%^&*_~ '''
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in data:
             if char not in punctuations:
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, "analyze.html", params)
+        data = analyzed
+        # return render(request, "analyze.html", params)
     #
     # TO UPPERCASE THE GIVEN TEXT
     #
-    elif(fullcaps == "on"):
+    if(fullcaps == "on"):
         analyzed = ""
         for char in data:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to uppercase', 'analyzed_text': analyzed}
-        return render(request, "analyze.html", params)
+        data = analyzed
+        # return render(request, "analyze.html", params)
     #
     # TO REMOVE NEW LINE CHARACTER FROM THE GIVEN STRING
     #
-    elif(newlineremover == "on"):
+    if(newlineremover == "on"):
         analyzed = ""
         for char in data:
-            if char != "\n":
+            if char != "\n" and char!= "\r":
                 analyzed = analyzed + char
         params = {'purpose': 'Removed new Line characters.', 'analyzed_text': analyzed}
-        return render(request, "analyze.html", params)
+        data = analyzed
+        # return render(request, "analyze.html", params)
     #
     # Remove Spaces
     #
-    elif(removespaces == "on"):
+    if(removespaces == "on"):
         analyzed = ""
         for char in data:
             if not char == " ":
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed any spaces', 'analyzed_text': analyzed}
-        return render(request, "analyze.html", params)
+        data = analyzed
+        # return render(request, "analyze.html", params)
     #
     # Count Characters
     #
     elif(countchar == "on"):
         counter = len(data)
         params = {'purpose': 'Counted characters', 'analyzed_text': counter}
-        return render(request, "analyze.html", params)
+        data = analyzed
+        # return render(request, "analyze.html", params)
 
-    else:
-        return HttpResponse("ERROR ")
+    return render(request, "analyze.html", params)
 
 def about(request):
     return render(request, "about.html")
